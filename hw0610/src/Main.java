eea694245a51fb8b3b501cb96efd69eab97c11aa
Akim Glazunov <glazunovakim@gmail.com>
2022-10-09 17:28:19 +0400

    Added task 2

--- hw0610/src/Main.java
+++ hw0610/src/Main.java
@@ -5,13 +5,31 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
         int a = input.nextInt();
-        if (10 > a && a >= 0){
-            System.out.println("DIGIT");
-        } else {
-            if (a >= 10 && a < 100){
-                System.out.println("NUM");
+        int b = input.nextInt();
+        int c = input.nextInt();
+        if (a < b && a < c){
+            if (b < c){
+                System.out.println(a + " " + b + " " + c);
             }else {
-                System.out.println("OTHER");
+                System.out.println(a + " " + c + " " + b);
+            }
+        }else {
+            if (b < a && b < c){
+                if (a < c){
+                    System.out.println(b + " " + a + " " + c);
+                }else {
+                    System.out.println(b + " " + c + " " + a);
+                }
+            }else {
+                if (c < a && c < b){
+                    if(a < b){
+                        System.out.println(c + " " + a + " " + b);
+                    }else {
+                        System.out.println(c + " " + b + " " + a);
+                    }
+                }else {
+
+                }
             }
         }
     }
